46d07a82c8286440c5fbf303db8faf99d87c447a
Abdulrahman Jama <abdirahman.jama010@gmail.com>
2024-10-19 06:48:19 +0300

    Make user able to input grid measurement
    made the same code for the func but used user input

--- myScript.js
+++ myScript.js
@@ -1,6 +1,12 @@
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
 
-function createGrid(row, column) {
+const btn = document.createElement('btn');
+btn.textContent = 'Change Grid';
+body.insertBefore(btn, container);
+btn.setAttribute('class', 'btn');
+
+let flexGrid = (row, column) => {
 let grid = row * column;
 
 for(let i = 0; i < grid; i++) {
@@ -11,10 +17,44 @@ for(let i = 0; i < grid; i++) {
     cell.addEventListener('mouseover', () => {
         cell.style.backgroundColor = 'blue';
         })
-        }
+
+}
+
 }
 
 
+flexGrid(16, 16);
 
-createGrid(16, 16);
 
+btn.addEventListener('click', () => {
+    container.remove();
+    let row = parseInt(prompt('enter squares per row'));
+    let  column = parseInt(prompt('enter number of squares per column'));
+    
+    if(row > 100 || column > 100) {
+        alert('the limit number of squares is 100, please insert a number below that.');
+    } else {
+        let grid = row * column;
+
+        const containerTwo = document.createElement('div');
+    containerTwo.setAttribute('class', 'containerTwo');
+   body.appendChild(containerTwo);
+
+   for(let i = 0; i < grid; i++) {
+    let cellTwo = document.createElement('div');
+
+    cellTwo.style.flexBasis = `${945 / row}px`;
+    cellTwo.style.height = `${945 / column}px`;
+     cellTwo.setAttribute('class', 'cellTwo');
+     containerTwo.appendChild(cellTwo);
+     
+ 
+     cellTwo.addEventListener('mouseover', () => {
+         cellTwo.style.backgroundColor = 'blue';
+         })
+ 
+ }
+    }
+    
+    
+})
